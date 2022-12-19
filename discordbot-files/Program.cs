315ffbd70dc4bdf8e42ff5fd3d56c84f6850a7e6
Dario Krieger <dario_krieger@sluz.ch>
2022-12-19 14:32:23 +0100

    initial push

---
+++ discordbot-files/Program.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord.Net;
+using Newtonsoft.Json;
+
+namespace discordbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, "ENTER YOUR DISCORD BOT TOKEN HERE");
+            await _client.StartAsync();
+
+            await _client.SetStatusAsync(UserStatus.DoNotDisturb);      // CAN BE SET TO ONLINE, IDLE, DND 
+            await _client.SetGameAsync("ENTER STATUS MESSAGE HERE");
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
